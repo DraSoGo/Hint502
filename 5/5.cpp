f9d1d421c2924b5ce9c3029c62a2ca28dc9a1460
DraSoGo <vdhfmfatv4321@gmail.com>
2024-05-08 20:11:09 +0700

    08/05/2024

---
+++ 5/5.cpp
@@ -0,0 +1,87 @@
+//เลื่อนลงไปด้านล่างสุดก่อน
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Graph
+{
+    int u,w;
+    bool operator < (const Graph & a)const
+    {
+        return w > a.w;
+    }
+};
+
+
+const int N = 1e5;
+int node[N],dist[N];
+priority_queue <Graph> PQ;
+vector <Graph> adj[N];
+
+void dijkstra(int st)
+{
+    dist[st] = 0;
+    PQ.push({st,0});
+    while (!PQ.empty())
+    {
+        auto [u,w] = PQ.top();
+        PQ.pop();
+        if (w > dist[u])
+        {
+            continue;
+        }
+        for (int i = 0; i < adj[u].size(); i++)
+        {
+            if (dist[adj[u][i].u] > w + adj[u][i].w)
+            {
+                PQ.push({adj[u][i].u,dist[adj[u][i].u] = w + adj[u][i].w});
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    fill(dist,dist + N,INT_MAX);
+    int n,e,u,v,w;
+    cin >> n >> e;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> node[i];
+    }
+    for (int i = 0; i < e; i++)
+    {
+        cin >> u >> v >> w;
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
+    }
+    dijkstra(0);
+    for (int i = 0; i < n; i++)
+    {
+        if (dist[i] >= 10)
+        {
+            cout << " ";
+        }
+        cout << char((node[i]-1)+'A') << " ";
+    }
+    cout << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cout << dist[i] << " ";
+    }
+    
+    return 0;
+}
+/*
+HHHBVFLUOAFDFGOGEPECHCDICBEBAB
+7 7
+7 1 21 5 11 20 14
+0 5 3
+5 6 14
+6 4 15
+4 2 7
+2 3 8
+2 1 4
+1 0 1
+*/
---
+++ 5/README.md
@@ -0,0 +1,18 @@
+# INPUT
+
+### โหลดโปรแกรมสำหรับ run code หรือ จะใช้แบบในเว็บก็ได้
+
+### โปรแกรม
+
+<img height = "100" src = "https://yt3.googleusercontent.com/_q52i8bUAEvcb7JR4e-eNTv23y2A_wg5sCz0NC0GrGtcw1CRMWJSOPVHUDh_bngD0q4gMvVeoA=s900-c-k-c0x00ffffff-no-rj">
+<img height = "100" src = "https://i0.wp.com/www.ba-na-na.net/wp-content/uploads/2016/10/codeblock-logo.png?resize=500%2C149">
+
+### |　vscode　　　　　　　codeblock　　　　　　　　　|
+
+### เว็บ
+
+- https://www.programiz.com/cpp-programming/online-compiler/ (แนะนำ)
+- https://www.onlinegdb.com/online_c++_compiler
+- https://onecompiler.com/cpp
+
+### นำโค้ดไป run โดยใช้ input เป็น `ANS2` จะได้ output ออกมาโดย output คือ `ANS5` (output มีแค่ 2 บรรทัดถ้าใช้เว็บ output คือ 2 บรรทัดสุดท้ายนะ)
